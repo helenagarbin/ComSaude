2e43048d8e1d0f60fc05550ea1994cf8ec82f741
Felipe Meireles Viegas <felipemeirelesviegas@gmail.com>
2025-10-03 00:14:47 -0300

    Adiciona pasta com-saude-front como parte do projeto

---
+++ Codigos/comSaude/src/main/java/ufcspa/comSaude/ComSaudeApplication.java
@@ -0,0 +1,13 @@
+package ufcspa.comSaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComSaudeApplication.class, args);
+	}
+
+}
---
+++ Codigos/comSaude/src/test/java/ufcspa/comSaude/ComSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package ufcspa.comSaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
